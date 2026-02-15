986d0403c527960762d44c02ac2c8f58ea66513e
nwlslms <nwlslms20@gmail.com>
2026-02-15 23:38:17 +0700

    None

--- DS-01/Pertemuan Keempat/dl.c
+++ DS-01/Pertemuan Keempat/dl.c
@@ -69,6 +69,8 @@ void pushTail(int val) {
         temp->prev = tail;
         tail = temp;
     }
+    print();
+
 }
 
 void popTail() {
@@ -83,12 +85,84 @@ void popTail() {
         free(temp);
         tail->next = NULL;
     }
+    print();
+}
+
+//Tengah / urut
+void pushMid(int val) {
+    if(!head) {
+        Node *temp = createNode(val);
+        head = tail = temp;
+    } else if (val <= head->val) {
+        pushHead(val);
+    } else if (val >= tail->val) {
+        pushTail(val);
+    } else {
+        Node *temp = createNode(val);
+        Node *curr = head;
+        while(curr->next && val >= curr->val) {
+            curr =curr->next;
+        }
+        // curr->prev->next = temp;
+        // temp->prev = curr->prev;
+        // temp->next = curr;
+        // curr->prev = temp;
+
+        Node *dump = curr->prev;
+        dump->next = temp;
+        temp->prev = dump;
+        temp->next = curr;
+        curr->prev = temp;
+    }
+    print();
+}
+
+void popMid(int val) {
+    if (!head) {
+        printf("Tidak ada data dalam list!\n");
+    } else if (head == tail) {
+        if(head->val = val) {
+            popHead();
+        } else {
+            printf("%d tidak ditemukan!\n", val);
+        }
+    } else {
+        Node *curr = head;
+        while(curr && curr->val != val) {
+            curr = curr->next;
+        }
+        if(!curr) {
+            printf("%d tidak ditemukan!\n", val);
+        } else {
+            if(val == head->val) {
+                popHead();
+            } else if (val == tail->val) {
+                popTail();
+            } else {
+                Node *kiri = curr->prev;
+                Node *kanan = curr->next;
+                kiri->next = kanan;
+                kanan->prev = kiri;
+                free(curr);
+            }
+        }
+    }
+    print();
 }
 int main () {
     print();
-    pushHead(12);
-    pushHead(10);
-    popHead();
-    popHead();
+    pushMid(10);
+    pushMid(10);
+    pushMid(12);
+    pushMid(6);
+    pushMid(7);
+    pushMid(8);
+    popMid(3);
+    popMid(10);
+    popMid(10);
+    popMid(6);
+    popMid(8);
+    popMid(7);
+    popMid(12);
     return 0;
 }
