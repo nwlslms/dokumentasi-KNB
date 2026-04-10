595945da1ce47a50f622cd13ae550be67a950429
Darwisy Ahmad A. <darwisyahmad54@gmail.com>
2026-04-10 10:33:57 +0700

    loops

---
+++ C-02/4/for.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    //    init;condition; increment
+    for (int i = 0; i < 10  ; i++) {
+      printf("Hello World! ke- %d\n", i);
+    }
+    
+    int j = 0;
+    while (j < 10){
+      printf("haloo ke- %d\n", j);
+      j++;
+    }
+}
